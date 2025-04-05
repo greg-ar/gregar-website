20736b9857ccd951d096b64959dbb53a7d85953e
Gregory Arapis <ga@gregar.dev>
2025-04-05 08:37:19 +0300

    First Portfolio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Gregory Arapis</h1>
+        <h2>FullStack Developer</h2>
+        <div style={{
+          display:'flex',
+          justifyContent:'space-between',
+          }}>
+          <img 
+            src='/media/portrait.jpeg' 
+            height='600px'
+          />
+          <ul>
+            <li>Javascript</li>
+            <li>HTML</li>
+            <li>php</li>
+            <li>CSS</li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>C/C++</li>
+            <li>Apache Tomcat</li>
+            <li>Linux Sysadmin</li>
+
+          </ul>
+        </div>
       </header>
     </div>
   );
